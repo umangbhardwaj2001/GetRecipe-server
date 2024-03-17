03f2bcaac4af77176390c243065470e1855e409d
Umang Bhardwaj <umangbhardwaj.ji@gmail.com>
2024-03-17 23:32:08 +0530

    add error papge

--- src/server.js
+++ src/server.js
@@ -10,6 +10,9 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/recipes", recipeRouter);
+app.use((req, res) => {
+  res.status(404).send("Sorry, the page you requested does not exist.");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
